bebc3326bd2eafcb85892505815618b039b69cb0
abhinav muthavarapu <abhinavmuthavarapu@gmail.com>
2020-07-19 22:50:07 +0530

    Netflix-clone

---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://seeklogo.com/images/N/netflix-logo-DD40971CE6-seeklogo.com.png"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Netflix avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
